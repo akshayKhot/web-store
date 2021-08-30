ee61c6cb206b83b1cbd4f0738afd9bb5300572ef
Akshay Khot <akshay.khot@hey.com>
2021-08-30 14:53:07 +0530

    working cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+  before_action :set_counter, only: %i[ index ]
+
   def index
+    session[:counter] += 1
+    @counter = session[:counter]
     @products = Product.order(:price)
+  end
+
+  def set_counter
+    session[:counter] ||= 0
   end
 end
