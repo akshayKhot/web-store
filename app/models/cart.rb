376efc7c02130a204492f8cecfb40ba8c3574717
Akshay Khot <akshay.khot@hey.com>
2021-08-31 11:01:04 +0530

    group products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,16 @@
 class Cart < ApplicationRecord
   # If you delete a cart, destroy all line items that belong to a cart
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product.id)
+    end
+
+    current_item
+  end
 end
